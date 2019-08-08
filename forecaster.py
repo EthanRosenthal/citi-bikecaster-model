5b09388505d66982b33e10fe4f4bb4554d02a00e
EthanRosenthal <ethan@ethanrosenthal.com>
2019-08-08 16:40:02 -0400

    POST events to insulator

--- forecaster.py
+++ forecaster.py
@@ -5,11 +5,16 @@ except ImportError:
 
 from io import StringIO
 import logging
+import os
 import pickle
+import time
 
 import boto3
+from datetime import datetime as dt
+from datetime import timedelta
 import numpy as np
 import pandas as pd
+import requests
 
 
 logger = logging.getLogger()
@@ -17,6 +22,9 @@ logger.setLevel(logging.INFO)
 
 s3 = boto3.resource("s3")
 BUCKET = "insulator-citi-bikecaster"
+USERNAME = os.environ["USERNAME"]
+API_KEY = os.environ["API_KEY"]
+INSULATOR_URL = "https://api-dev.insulator.ai/v1/time_series"
 
 
 def model_key(station_id):
@@ -43,6 +51,33 @@ def ts_to_unixtime(series):
     return series.astype(np.int64) // 10 ** 9
 
 
+def post_outcome(df, station_id):
+    two_hours_ago = dt.now() - timedelta(hours=2)
+    past_two_hours = df[df["last_reported"] >= two_hours_ago]
+    past_two_hours = past_two_hours.sort_values("last_reported")
+
+    series_timestamps = ts_to_unixtime(past_two_hours["last_reported"]).tolist()
+    series_values = past_two_hours["num_bikes_available"].astype("int").tolist()
+    post_event(station_id, series_timestamps, series_values, "outcome")
+
+
+def post_event(station_id, series_timestamps, series_values, event_type):
+    payload = {
+        "service_name": "bikecaster",
+        "model_name": "lin_reg",
+        "model_version": "0.1.0",
+        "timestamp": time.time(),
+        "entities": {"station_id": station_id},
+        "series_timestamps": series_timestamps,
+        "series_values": series_values
+    }
+    assert event_type in ("prediction", "outcome")
+    url = f"{INSULATOR_URL}/{event_type}"
+    response = requests.post(url, auth=(USERNAME, API_KEY), json=payload)
+    if not response:
+        logger.error(f"Error posting to insulator ingest API: {response.text}")
+
+
 def handler(event, context):
     logger.info("Download data cache")
     df = load_data_cache()
@@ -55,6 +90,9 @@ def handler(event, context):
 
     df = df[df["station_id"] == station_id]
     df["last_reported"] = pd.to_datetime(df["last_reported"])
+
+    post_outcome(df, station_id)
+
     df = (
         df
         .set_index("last_reported")
@@ -69,12 +107,12 @@ def handler(event, context):
     model = load_model(station_id)
 
     series_values = np.squeeze(model.predict(X, start_idx=len(X) - 1))
-    series_values = np.clip(series_values.astype(int), 0, None)
+    series_values = np.clip(series_values.astype(int), 0, None).astype("int").tolist()
     series_timestamps = pd.date_range(
         df.index[-1], periods=len(series_values) + 1, freq="5T"
     )
     # Remove the first value because it's the last value in the original data.
     series_timestamps = series_timestamps[1:]
-    series_timestamps = ts_to_unixtime(series_timestamps)
-    logger.info({"series_values": series_values, "series_timestamps": series_timestamps})
+    series_timestamps = ts_to_unixtime(series_timestamps).astype("int").tolist()
+    post_event(station_id, series_timestamps, series_values, "prediction")
     return "OK"
