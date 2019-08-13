19fff58847c3d15bfb28282f698fae537e6ad89c
EthanRosenthal <ethan@ethanrosenthal.com>
2019-08-13 13:07:02 -0400

    Set path instead of activating environment

--- scripts/dask_scheduler.sh
+++ scripts/dask_scheduler.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
+PATH=/home/ubuntu/miniconda3/envs/citi_bikecaster_model/bin/:$PATH
+
 dask-scheduler --host 192.168.0.140 --port 8786
--- scripts/dask_workers.sh
+++ scripts/dask_workers.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
+PATH=/home/ubuntu/miniconda3/envs/citi_bikecaster_model/bin/:$PATH
+
 dask-worker --nthreads 1 --nprocs 2 --memory-limit "1.7GB" tcp://192.168.0.140:8786
