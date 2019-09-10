3163cb7074c8f6618138220d0476b2d1e778a2b9
EthanRosenthal <ethan@ethanrosenthal.com>
2019-09-10 16:03:34 -0400

    Add multiple endpoints

--- calcs.py
+++ calcs.py
@@ -12,7 +12,10 @@ import s3fs
 
 
 BUCKET = "insulator-citi-bikecaster"
-INSULATOR_URL = "https://api-dev.insulator.ai/v1/time_series"
+INSULATOR_URLS = [
+    "https://api-dev.insulator.ai/v1/time_series",
+    "https://ybcbwoz3w6.execute-api.us-east-1.amazonaws.com/staging/v1/time_series"
+]
 
 s3 = s3fs.S3FileSystem()
 
@@ -35,17 +38,17 @@ def ts_to_unixtime(series):
     return series.astype(np.int64) // 10 ** 9
 
 
-def post_outcome(df, station_id, username, api_key):
+def post_outcome(df, station_id, usernames, api_keys):
     two_hours_ago = dt.now() - timedelta(hours=2)
     past_two_hours = df[df["last_reported"] >= two_hours_ago]
     past_two_hours = past_two_hours.sort_values("last_reported")
 
     series_timestamps = ts_to_unixtime(past_two_hours["last_reported"]).tolist()
     series_values = past_two_hours["num_bikes_available"].astype("int").tolist()
-    post_event(station_id, series_timestamps, series_values, "outcome", username, api_key)
+    post_event(station_id, series_timestamps, series_values, "outcome", usernames, api_keys)
 
 
-def post_event(station_id, series_timestamps, series_values, event_type, username, api_key):
+def post_event(station_id, series_timestamps, series_values, event_type, usernames, api_keys):
     payload = {
         "service_name": "bikecaster",
         "model_name": "lin_reg",
@@ -56,16 +59,20 @@ def post_event(station_id, series_timestamps, series_values, event_type, usernam
         "series_values": series_values
     }
     assert event_type in ("prediction", "outcome")
-    url = f"{INSULATOR_URL}/{event_type}"
-    response = requests.post(url, auth=(username, api_key), json=payload)
-    if not response:
-        print(f"Error posting to insulator ingest API: {response.text}")
+    for username, api_key, insulator_url in zip(usernames, api_keys, INSULATOR_URLS):
+        url = f"{insulator_url}/{event_type}"
+        try:
+            response = requests.post(url, auth=(username, api_key), json=payload)
+            if not response:
+                print(f"Error posting to insulator ingest API: {response.text}")
+        except Exception as e:
+            print(e)
 
 
-def make_forecast(df, station_id, username, api_key):
+def make_forecast(df, station_id, usernames, api_keys):
     station_df = df[df["station_id"] == station_id]
 
-    post_outcome(station_df, station_id, username, api_key)
+    post_outcome(station_df, station_id, usernames, api_keys)
 
     station_df = (
         station_df
@@ -97,11 +104,11 @@ def make_forecast(df, station_id, username, api_key):
     # Remove the first value because it's the last value in the original data.
     series_timestamps = series_timestamps[1:]
     series_timestamps = ts_to_unixtime(series_timestamps).astype("int").tolist()
-    post_event(station_id, series_timestamps, series_values, "prediction", username, api_key)
+    post_event(station_id, series_timestamps, series_values, "prediction", usernames, api_keys)
     return True
 
 
-def pipeline(s3_path, username, api_key):
+def pipeline(s3_path, usernames, api_keys):
     df = dd.read_csv(s3_path).compute()
     df["last_reported"] = pd.to_datetime(df["last_reported"])
     MIN_DATE = "2016-01-01"
@@ -110,7 +117,7 @@ def pipeline(s3_path, username, api_key):
         df_future = client.scatter(df)
         futures = []
         for station_id in sorted(df["station_id"].unique().tolist()):
-            futures.append(client.submit(make_forecast, df_future, station_id, username, api_key))
+            futures.append(client.submit(make_forecast, df_future, station_id, usernames, api_keys))
         total = len(futures)
         success = 0
         for result in as_completed(futures):
--- forecaster/views.py
+++ forecaster/views.py
@@ -1,24 +1,22 @@
 import os
 from pathlib import Path
 
-import dask.dataframe as dd
 from dask.distributed import fire_and_forget
-import pandas as pd
 
 from forecaster import app
 from calcs import pipeline
 
 
 BUCKET = "insulator-citi-bikecaster"
-USERNAME = os.environ["USERNAME"]
-API_KEY = os.environ["API_KEY"]
+USERNAMES = os.environ["USERNAMES"].split(",")
+API_KEYS = os.environ["API_KEYS"].split(",")
 
 
 @app.route("/", methods=["POST",])
 def post():
     app.dask_client.upload_file(Path(__file__).absolute().parent.parent / "calcs.py")
     s3_path = f"s3://{BUCKET}/status_cache.csv"
-    fire_and_forget(app.dask_client.submit(pipeline, s3_path, USERNAME, API_KEY))
+    fire_and_forget(app.dask_client.submit(pipeline, s3_path, USERNAMES, API_KEYS))
     return "OK"
 
 
